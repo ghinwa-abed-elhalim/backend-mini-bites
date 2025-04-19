1ca93e10f2f60cfc3d9a978acc741df543721c00
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-19 15:15:24 +0100

    tasks added

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+$tasks = [
+    ["id" => 1, "title" => "Submit backend web project", "status" => "pending", "due"=> "19-04-2025"],
+    ["id" => 2, "title" => "Go shopping", "status" => "done", "due"=> "18-04-2025"],
+    ["id" => 3, "title" => "Submit final web project", "status" => "pending", "due"=> "21-04-2025"],
+    ["id" => 4, "title" => "Go for a walk", "status" => "done", "due"=> "17-04-2025"]
+];
 ?>
